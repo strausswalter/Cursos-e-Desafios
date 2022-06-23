74449b269d4ec781f4bb34d03acbfbb23fd06195
strausswalter <strausswalter@gmail.com>
2022-06-23 19:05:25 -0300

    .

---
+++ Infnet/I230 - Fundamentos de Programação JavaScript/exemplo.02.js
@@ -0,0 +1,24 @@
+
+
+
+
+function CalculaMedia (nota1, nota2){
+    media = (nota1+nota2)/2;
+    return media;
+
+}
+
+let notaFinal=CalculaMedia();
+
+// ========================
+
+
+function SomaValores(a, b){
+    total = a+b;
+    // Retorna um valor
+    return total;
+}
+
+_ValorCarro = 5340;
+valorTotal = SomaValores (1750, _ValorCarro); //7090
+
